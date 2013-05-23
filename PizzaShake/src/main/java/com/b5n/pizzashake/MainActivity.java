7c8336cd798fcab512e54b10f53694b4933a3083
Bastien Colmard <baztoune@gmail.com>
2013-05-23 22:55:08 +0200

    images

--- PizzaShake/src/main/java/com/b5n/pizzashake/MainActivity.java
+++ PizzaShake/src/main/java/com/b5n/pizzashake/MainActivity.java
@@ -1,29 +1,29 @@
 package com.b5n.pizzashake;
 
 import android.app.Activity;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.Menu;
-
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
-import com.squareup.seismic.ShakeDetector;
+
+import static android.view.Gravity.CENTER;
+import static android.view.ViewGroup.LayoutParams;
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
 public class MainActivity extends Activity implements ShakeDetector.Listener {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        ShakeDetector sd = new ShakeDetector(this);
+        sd.start(sensorManager);
     }
 
     public void hearShake() {
-        Toast.makeText(this, "Don't shake me, bro!", Toast.LENGTH_SHORT).show();
+        ImageView image = (ImageView) findViewById(R.id.imageView2);
+        image.setImageResource(R.drawable.pizzahut2);
     }
 }
